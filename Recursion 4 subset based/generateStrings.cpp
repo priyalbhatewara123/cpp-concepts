9b34477b3a3dcec1712c84a298bc43a00fceaae7
priyal bhatewara <47683250+priyalbhatewara123@users.noreply.github.com>
2020-10-13 08:36:05 +0530

    Modified generate strings

--- Recursion 4 subset based/generateStrings.cpp
+++ Recursion 4 subset based/generateStrings.cpp
@@ -14,16 +14,32 @@ void generate_strings(char *in , char *out, int i, int j) {
 
 	//recursive case
 	//one digit at a time
-	int digit = in[i] - '0';
-	char ch = digit + 'A' - 1;
+	int digit = 0;
+	char ch;
+	if (in[i] == '0') {    //to handle single zero
+		generate_strings(in, out, i + 1, j);
+		return;     //to avoid execution of steps given below
+	}
+	else {
+		digit = in[i] - '0';
+		ch = digit + 'A' - 1;
 
-	out[j] = ch;
-	generate_strings(in, out, i + 1, j + 1);
+		out[j] = ch;
+		generate_strings(in, out, i + 1, j + 1);
+	}
 
 	//two digit at a time
 	if (in[i + 1] != '\0') {
-		int secondDigit = in[i + 1] - '0';
-		int no = digit * 10 + secondDigit;
+
+		int no = 0;
+
+		if (in[i + 1] == '0') {   //for handling 0
+			no = digit * 10;
+		}
+		else {
+			int secondDigit = in[i + 1] - '0';
+			no = digit * 10 + secondDigit;
+		}
 
 		if (no <= 26) {
 			ch = no + 'A' - 1;
